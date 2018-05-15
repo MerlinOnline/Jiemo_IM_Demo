ce1aef54b09881f87abb9cf3efcad90f4b813855
merrill <243203681@qq.com>
2018-05-15 13:46:53 +0800

    Initial Commit

---
+++ Jiemo_IM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jiemo_IM
+//
+//  Created by merrill on 2018/5/15.
+//  Copyright © 2018年 Tim2018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Jiemo_IM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jiemo_IM
+//
+//  Created by merrill on 2018/5/15.
+//  Copyright © 2018年 Tim2018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
